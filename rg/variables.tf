362c78216434ee1fc617ac95726d8b0f6c7c650f
KomalGill023 <87756956+KomalGill023@users.noreply.github.com>
2021-09-22 15:45:43 +0530

    Update variables.tf

--- rg/variables.tf
+++ rg/variables.tf
@@ -1,8 +1,8 @@
-variable "resource-roup-name" {
+variable "resource-group-name" {
   type=string
   default="Testrgpipeline"
 }
-variable "Location" {
+variable "location" {
   type=string
   default="West Europe"
 }
