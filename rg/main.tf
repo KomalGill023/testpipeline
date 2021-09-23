1cf1ca7eeef6957b06a37007bfaf7d197d36e14a
KomalGill023 <87756956+KomalGill023@users.noreply.github.com>
2021-09-23 11:09:46 +0530

    Update main.tf

--- rg/main.tf
+++ rg/main.tf
@@ -1,22 +1,18 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=0.11"
-    }
-  }
+  required_version = ">=0.11"
+   
+  
    backend "azurerm" {
         resource_group_name  = "test-terra"
         storage_account_name = "teststateterra"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
     }
-    provider "azurerm" {
+}
+
+provider "azurerm" {
 features {}
 }
-
-}
-
 
 resource "azurerm_resource_group" "example" {
   name     = "${var.resource-group-name}"
